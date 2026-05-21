898397e2b7561b4df8401d6bc7008ea5ad32790a
angelojuliocome5-lang <angelojuliocome5@gmail.com>
2026-05-21 22:16:23 +0200

    Add bot monitoring functionality in app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+import time
+
+def rodar_bot():
+    print("O Robô Comé Solutions está operando...")
+    while True:
+        print("Monitorando a plataforma...")
+        time.sleep(60)
+
+if __name__ == "__main__":
+    rodar_bot()
